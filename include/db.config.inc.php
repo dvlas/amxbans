326194bb34195d2e7f791f237e96f382eb42fd98
dvlas <tier6969@gmail.com>
2019-12-21 02:12:47 +0300

    Git init

---
+++ include/db.config.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+	$config->document_root = "";
+	$config->path_root = "D:\WEBSERVER\domains\amxbans6.14.4";
+
+	$config->db_host = "127.0.0.1";
+	$config->db_user = "root";
+	$config->db_pass = "";
+	$config->db_db = "amxbans";
+	$config->db_prefix = "amx";
+	
+?>
